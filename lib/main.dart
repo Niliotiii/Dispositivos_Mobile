240e03e29083ecbd7f507095a88b7ed7164084c3
niliotiii <d.saiter@estudante.ifro.edu.br>
2024-04-17 19:35:21 -0400

    feat: add page login and integrado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'app/pages/home.dart';
-import 'app/pages/book-details.dart';
+import 'app/pages/login.dart';
+import 'app/pages/integrado.dart';
 
 void main() {
   runApp(Main());
@@ -12,48 +12,34 @@ class Main extends StatelessWidget {
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           centerTitle: true,
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.white,
           titleTextStyle: TextStyle(
               color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
         ),
         textTheme: const TextTheme(
           bodyMedium: TextStyle(
-            fontSize: 20,
+            fontSize: 30,
             color: Colors.black,
           ),
         ),
       ),
       onGenerateRoute: (settings) {
-        if (settings.name == '/bookDetails') {
-          final BookDetails args = settings.arguments as BookDetails;
-
-          return MaterialPageRoute(
-            builder: (context) => BookDetails(
-              book: args.book,
-              releaseDate: args.releaseDate,
-              coverImageUrl: args.coverImageUrl,
-              synopsis: args.synopsis,
-              author: args.author,
-              authorBiography: args.authorBiography,
-              publisher: args.publisher,
-            ),
-          );
-        }
-        // Define as outras rotas como antes
         return MaterialPageRoute(
           builder: (context) {
             switch (settings.name) {
               case '/main':
                 return Main();
-              case '/home':
-                return Home();
+              case '/login':
+                return Login();
+              case '/integrado':
+                return Integrado();
               default:
-                return Home();
+                return Login();
             }
           },
         );
       },
-      initialRoute: '/home',
+      initialRoute: '/login',
     );
   }
 }
